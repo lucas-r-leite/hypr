d0748084b090c65d8e002777f364de8890598cc5
lucas leite <lucasro644@gmail.com>
2026-01-14 12:15:29 -0300

    :hammer: install script

---
+++ hypr_install.sh
@@ -0,0 +1,34 @@
+echo "### Installing Wayland + Hyprland stack ###"
+sudo pacman -S --needed --noconfirm \
+  hyprland \
+  hyprlock \
+  hypridle \
+  hyprctl \
+  hyprpaper \
+  hyprsunset \
+  waybar \
+  wofi \
+  swaync \
+  grim \
+  slurp \
+  xdg-desktop-portal-hyprland \
+  xdg-desktop-portal \
+  pipewire \
+  pipewire-alsa \
+  pipewire-pulse \
+  pipewire-jack \
+  wireplumber \
+  wl-clipboard \
+  cliphist \
+  pavucontrol \
+  brightnessctl \
+  lxappearance \
+  qt5ct \
+  qt6ct \
+  qt5-wayland \
+  qt6-wayland \
+  gnome-themes-extra \
+  ttf-tinos-nerd \
+  networkmanager \
+  blueman \
+  libsecret
